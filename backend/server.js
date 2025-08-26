142bd0b6c7e0ea17cb2848e94ccc8a3943a6b8df
joenash156 <joenash156@gmail.com>
2025-08-26 22:11:14 +0000

    Refactor package.json and add server.js for backend setup

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
-const expensesRoutes = require("../routes/expenses-routes.js");
+const expensesRoutes = require("./routes/expenses-routes.js");
 
 app.use(cors());
 app.use(express.json());
